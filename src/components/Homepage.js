a31f0f9b6718093f85900e61ced186f31fad1b73
SnowSurfer <drinkaziu@gmail.com>
2018-08-18 21:24:49 -0400

    create and style homepage

---
+++ src/components/Homepage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Homepage = () => (
+  <div className="home-hero">
+    <h1>What's Chirpin'</h1>
+  </div>
+)
+
+export default Homepage;
--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -1,53 +1,34 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
-import { Segment, Menu, Container, Button, Icon } from "semantic-ui-react";
+import { Menu, Container, Button, Icon } from "semantic-ui-react";
 
 class Navbar extends Component {
-  state = {}
-
-  hideFixedMenu = () => this.setState({ fixed: false })
-  showFixedMenu = () => this.setState({ fixed: true })
 
   render() {
 
-    const { fixed } = this.state
-
     return(
-      <Segment
-        inverted
-        textAlign='center'
-        style={{ minHeight: 700, padding: '1em 0em' }}
-        vertical
-      >
-        <Menu
-          fixed={fixed ? 'top' : null}
-          inverted={!fixed}
-          pointing={!fixed}
-          secondary={!fixed}
-          size='large'
-        >
-          <Container>
-            <Link to="/">
-              <Menu.Item>
-                <Icon name="earlybirds" size="big"/>
-              </Menu.Item>
-            </Link>
-            <Menu.Item position='right'>
-              <Link to="/signin">
-                <Button inverted={!fixed}>
-                  Log in
-                </Button>
-              </Link>
-              <Link to="/signup">
-                <Button inverted={!fixed} primary={fixed} style={{ marginLeft: '0.5em' }}>
-                  Sign Up
-                </Button>
-              </Link>
+      <Menu pointing secondary size='large' className="navbar">
+        <Container>
+          <Link to="/">
+            <Menu.Item>
+              <Icon name="earlybirds" size="big"/>
             </Menu.Item>
-          </Container>
-        </Menu>
-      </Segment>
+          </Link>
+          <Menu.Item position='right'>
+            <Link to="/signin">
+              <Button >
+                Log in
+              </Button>
+            </Link>
+            <Link to="/signup">
+              <Button style={{ marginLeft: '0.5em' }}>
+                Sign Up
+              </Button>
+            </Link>
+          </Menu.Item>
+        </Container>
+      </Menu>
     )
   }
 }
